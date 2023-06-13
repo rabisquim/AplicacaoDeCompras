06fcfc3e251774bae24b1d8f16ed560e659227dc
Robson Paulo <65135918+rabisquim@users.noreply.github.com>
2023-06-13 20:06:53 -0300

    Update Principal.java

--- src/br/com/alura/aplicacaodecompras/principal/Principal.java
+++ src/br/com/alura/aplicacaodecompras/principal/Principal.java
@@ -1,17 +1,57 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+package br.com.alura.aplicacaodecompras.principal;
+
+import br.com.alura.aplicacaodecompras.modelos.Produtos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//Jaraguá do Sul, SC - Brasil | 2023 | Alura
+
+public class Principal {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        List<Produtos> compras = new ArrayList<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Informe o valor do Limite do Cartão (em R$): ");
+        double limite = scan.nextDouble();
+        scan.nextLine(); //Limpar o buffer do scanner
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        while (true) {
+            System.out.println("Informe o [NOME] do item (ou 'sair' para finalizar): ");
+            String nome = scan.nextLine();
+
+            if (nome.equals("sair")) {
+                break;
+            }
+            System.out.println("Informe o valor do item (em R$: ");
+            double valor = scan.nextDouble();
+            scan.nextLine(); //Limpar o buffer do scanner
+
+            if (valor > limite) {
+                System.out.printf("Limite do Cartão excedido. Saldo do cartão: R$ %.2f ", limite);
+                System.out.println();
+                continue;
+            }
+
+            compras.add(new Produtos(nome, valor));
+            limite -= valor;
+            System.out.printf("Item adicionado à lista de compras. Saldo do cartão: R$ %.2f%n", limite);
+            System.out.println();
         }
+        //Ordenar as compras em ordem crescente de valor
+        compras.sort((p1, p2) -> Double.compare(p1.getPreco(), p2.getPreco()));
+        System.out.println();
+        System.out.println("COMPRAS REALIZADAS: ");
+        System.out.println("========================================");
+        for (Produtos produto : compras) {
+            System.out.printf("%s - R$ %.2f", produto.getNome(), produto.getPreco());
+            System.out.println();
+
+        }
+        System.out.println("========================================");
+        System.out.printf("Saldo do cartão: R$ %.2f%n", limite);
+        System.out.println("========================================");
+
     }
 }
