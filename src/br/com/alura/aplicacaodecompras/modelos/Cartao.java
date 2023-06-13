88c64c03d3821f4e73f9337f1476218ee27df43c
Robson Paulo <65135918+rabisquim@users.noreply.github.com>
2023-06-13 20:07:50 -0300

    Update Cartao.java

--- src/br/com/alura/aplicacaodecompras/modelos/Cartao.java
+++ src/br/com/alura/aplicacaodecompras/modelos/Cartao.java
@@ -1,2 +1,13 @@
-package br.com.alura.aplicacaodecompras.modelos;public class Cartao {
+package br.com.alura.aplicacaodecompras.modelos;
+
+public class Cartao {
+    private double limite;
+
+    public Cartao(double limite) {
+        this.limite = limite;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
 }
